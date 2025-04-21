4ef5fd0fc97a3c407956cef775235de8ea206cd9
Brian Salomone <brian.j.salomone@gmail.com>
2025-04-20 20:06:38 -0700

    added circular progress indicator before list update on launch

--- app/src/main/java/amazon/fetch/test/MainActivity.kt
+++ app/src/main/java/amazon/fetch/test/MainActivity.kt
@@ -10,10 +10,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -109,8 +112,16 @@ fun ItemList() {
     }
 
     Column(modifier = Modifier
-        .background(colorResource(R.color.grey_300))) {
-        ListDisplay(updatedItemList)
+        .fillMaxSize()
+        .background(colorResource(R.color.grey_300)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        if (updatedItemList.isEmpty()) {
+            CircularProgressIndicator()
+        }
+        Column {
+            ListDisplay(updatedItemList)
+        }
     }
 }
 
